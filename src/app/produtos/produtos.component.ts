67aa934ddfc20a1790d19417c8635eb35816c0a1
neeivaCosta <neiva.cmartins@gmail.com>
2022-10-24 17:55:05 -0300

    projeto angular

---
+++ src/app/produtos/produtos.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ProdutosService } from '../produto.service';
+import { IProduto } from '../produtos';
+
+@Component({
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrls: ['./produtos.component.css']
+})
+export class ProdutosComponent implements OnInit {
+  produtos: IProduto[] |  undefined;
+
+  constructor(
+    private produtosService: ProdutosService
+  ) { }
+
+  ngOnInit(): void {
+    this.produtos = this.produtosService.getAll();
+  }
+
+}
